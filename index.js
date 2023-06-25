0b23ffe6cd7283c553e96af3255eb041cbc645d8
Taylor Curry <currytay@gmail.com>
2023-06-25 18:20:37 +1000

    Convert to express

--- index.js
+++ index.js
@@ -1,45 +1,23 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const app = express();
+const fs = require("fs");
+const port = 3000;
 
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
-
-  // set header content type
-  res.setHeader('Content-Type', 'text/html');
-
-  let path = './';
-  switch(req.url) {
-    case '/':
-      path += 'index.html';
-      res.statusCode = 200;
-      break;
-    case '/about':
-      path += 'about.html';
-      res.statusCode = 200;
-      break;
-    case '/contact':
-      path += 'contact-me.html';
-      res.statusCode = 200;
-      break;
-    default:
-      path += '404.html';
-      res.statusCode = 404;
-      break;
-  }
-
-  // send html file
-  fs.readFile(path, (err, data) => {
-    if (err) {
-      console.log(err);
-      res.end();
-    } else {
-      // res.write(data);
-      res.end(data);
-    }
-  })
+// Welcome page
+app.get("/", function (req, res) {
+  res.sendFile("index.html", {root: __dirname });
 });
 
-server.listen(3000, 'localhost', () => {
-  console.log('listening for requests on port 3000!');
+// About page
+app.get("/about", function (req, res) {
+  res.sendFile("about.html", {root: __dirname });
 });
 
+// Contact page
+app.get("/contact", function (req, res) {
+  res.sendFile("contact-me.html", {root: __dirname });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
